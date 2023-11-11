1ccc5318a8a7a4ef688b4b2dfeb7b5406c67235c
Håvar Aambø Fosstveit <havar@fosstveit.net>
2023-11-11 22:08:59 +0100

    Update bufferline config

--- lua/havfo/plugins/bufferline.lua
+++ lua/havfo/plugins/bufferline.lua
@@ -3,12 +3,14 @@ return {
 	version = '*',
 	dependencies = { 'nvim-tree/nvim-web-devicons', 'echasnovski/mini.bufremove' },
 	config = function()
+		local bufremove = require('mini.bufremove')
+
 		require('bufferline').setup({
 			options = {
 				-- stylua: ignore
-				close_command = function(n) require('mini.bufremove').delete(n, false) end,
+				close_command = function(n) bufremove.delete(n, false) end,
 				-- stylua: ignore
-				right_mouse_command = function(n) require('mini.bufremove').delete(n, false) end,
+				right_mouse_command = function(n) bufremove.delete(n, false) end,
 				diagnostics = 'nvim_lsp',
 				always_show_bufferline = false,
 				diagnostics_indicator = function(_, _, diag)
@@ -26,5 +28,19 @@ return {
 				},
 			},
 		})
+
+		vim.keymap.set('n', '<leader>bc', bufremove.delete, { noremap = true, silent = true })
+		vim.keymap.set('n', '<leader>bo', ':BufferLineCloseOthers<cr>', { noremap = true, silent = true })
+		vim.keymap.set('n', '<leader>bn', ':BufferLineCycleNext<cr>', { noremap = true, silent = true })
+		vim.keymap.set('n', '<leader>bp', ':BufferLineCyclePrev<cr>', { noremap = true, silent = true })
+		vim.keymap.set('n', '<leader>1', ':BufferLineGoToBuffer 1<cr>', { noremap = true, silent = true })
+		vim.keymap.set('n', '<leader>2', ':BufferLineGoToBuffer 2<cr>', { noremap = true, silent = true })
+		vim.keymap.set('n', '<leader>3', ':BufferLineGoToBuffer 3<cr>', { noremap = true, silent = true })
+		vim.keymap.set('n', '<leader>4', ':BufferLineGoToBuffer 4<cr>', { noremap = true, silent = true })
+		vim.keymap.set('n', '<leader>5', ':BufferLineGoToBuffer 5<cr>', { noremap = true, silent = true })
+		vim.keymap.set('n', '<leader>6', ':BufferLineGoToBuffer 6<cr>', { noremap = true, silent = true })
+		vim.keymap.set('n', '<leader>7', ':BufferLineGoToBuffer 7<cr>', { noremap = true, silent = true })
+		vim.keymap.set('n', '<leader>8', ':BufferLineGoToBuffer 8<cr>', { noremap = true, silent = true })
+		vim.keymap.set('n', '<leader>9', ':BufferLineGoToBuffer 9<cr>', { noremap = true, silent = true })
 	end
 }
