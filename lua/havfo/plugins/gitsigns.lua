070ce718367ed8c917b18fed6a96b509a24f1c39
Håvar Aambø Fosstveit <havar@fosstveit.net>
2023-12-11 22:49:15 +0100

    Update gitsigns

--- lua/havfo/plugins/gitsigns.lua
+++ lua/havfo/plugins/gitsigns.lua
@@ -9,9 +9,19 @@ return {
 			changedelete = { text = '~' },
 		},
 		on_attach = function(bufnr)
-			vim.keymap.set('n', '<leader>gh', require('gitsigns').preview_hunk,
-				{ buffer = bufnr, desc = 'Preview git hunk' })
 			local gs = package.loaded.gitsigns
+
+			vim.keymap.set('n', '<leader>gbl', gs.toggle_current_line_blame, { buffer = bufnr, desc = 'Blame line' })
+			vim.keymap.set('n', '<leader>gbf', function() gs.blame_line{ full=true } end, { buffer = bufnr, desc = 'Blame file' })
+
+			vim.keymap.set('n', '<leader>gs', gs.stage_hunk, { buffer = bufnr, desc = 'Stage hunk' })
+			vim.keymap.set('n', '<leader>gr', gs.reset_hunk, { buffer = bufnr, desc = 'Reset hunk' })
+			vim.keymap.set('n', '<leader>gu', gs.undo_stage_hunk, { buffer = bufnr, desc = 'Undo stage hunk' })
+
+			vim.keymap.set('v', '<leader>gs', function() gs.stage_hunk {vim.fn.line('.'), vim.fn.line('v')} end, { buffer = bufnr, desc = 'Stage hunk' })
+			vim.keymap.set('v', '<leader>gr', function() gs.reset_hunk {vim.fn.line('.'), vim.fn.line('v')} end, { buffer = bufnr, desc = 'Reset hunk' })
+
+			vim.keymap.set('n', '<leader>gh', gs.preview_hunk, { buffer = bufnr, desc = 'Preview git hunk' })
 			vim.keymap.set({ 'n', 'v' }, ']c', function()
 				if vim.wo.diff then
 					return ']c'
