926a1b2a72c1c70a5e078dc5f103b5eabfecc84c
Håvar Aambø Fosstveit <havar@fosstveit.net>
2024-02-10 19:44:57 +0100

    Add descriptions

--- lua/havfo/plugins/package.lua
+++ lua/havfo/plugins/package.lua
@@ -7,24 +7,24 @@ return {
 		require("package-info").setup()
 
 		-- Show dependency versions
-		vim.keymap.set({ "n" }, "<leader>ns", require("package-info").show, { silent = true, noremap = true })
+		vim.keymap.set({ "n" }, "<leader>ns", require("package-info").show, { desc = "Show versions", silent = true, noremap = true })
 
 		-- Hide dependency versions
-		vim.keymap.set({ "n" }, "<leader>nc", require("package-info").hide, { silent = true, noremap = true })
+		vim.keymap.set({ "n" }, "<leader>nc", require("package-info").hide, { desc = "Hide versions", silent = true, noremap = true })
 
 		-- Toggle dependency versions
-		vim.keymap.set({ "n" }, "<leader>nt", require("package-info").toggle, { silent = true, noremap = true })
+		vim.keymap.set({ "n" }, "<leader>nt", require("package-info").toggle, { desc = "Toggle versions", silent = true, noremap = true })
 
 		-- Update dependency on the line
-		vim.keymap.set({ "n" }, "<leader>nu", require("package-info").update, { silent = true, noremap = true })
+		vim.keymap.set({ "n" }, "<leader>nu", require("package-info").update, { desc = "Update version on line", silent = true, noremap = true })
 
 		-- Delete dependency on the line
-		vim.keymap.set({ "n" }, "<leader>nd", require("package-info").delete, { silent = true, noremap = true })
+		vim.keymap.set({ "n" }, "<leader>nd", require("package-info").delete, { desc = "Delete dependency", silent = true, noremap = true })
 
 		-- Install a new dependency
-		vim.keymap.set({ "n" }, "<leader>ni", require("package-info").install, { silent = true, noremap = true })
+		vim.keymap.set({ "n" }, "<leader>ni", require("package-info").install, { desc = "Install dependency", silent = true, noremap = true })
 
 		-- Install a different dependency version
-		vim.keymap.set({ "n" }, "<leader>np", require("package-info").change_version, { silent = true, noremap = true })
+		vim.keymap.set({ "n" }, "<leader>np", require("package-info").change_version, { desc = "Install different version", silent = true, noremap = true })
 	end
 }
