3bb01acaf40353afe6cd236efdb722e693c047a3
Håvar Aambø Fosstveit <havar@fosstveit.net>
2024-03-20 20:46:32 +0100

    Remap leap

--- lua/havfo/plugins/leap.lua
+++ lua/havfo/plugins/leap.lua
@@ -1,8 +1,8 @@
 return {
 	'ggandor/leap.nvim',
 	config = function()
-		vim.keymap.set({'n', 'x', 'o'}, 'f',  '<Plug>(leap-forward)')
-		vim.keymap.set({'n', 'x', 'o'}, 'F',  '<Plug>(leap-backward)')
-		vim.keymap.set({'n', 'x', 'o'}, 'gf', '<Plug>(leap-from-window)')
+		vim.keymap.set({'n', 'x', 'o'}, 's',  '<Plug>(leap-forward)')
+		vim.keymap.set({'n', 'x', 'o'}, 'S',  '<Plug>(leap-backward)')
+		vim.keymap.set({'n', 'x', 'o'}, 'gs', '<Plug>(leap-from-window)')
 	end,
 }
